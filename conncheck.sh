127c28fd23943f7b7129ff6c73d0e40f5f0cfbf9
stellerstudent <stellerstudent@users.noreply.github.com>
2015-02-16 14:17:22 -0600

    Removed ambiguity in netstat loop
    only supports tcp6 connections
    
    Added # to comments in log file

--- conncheck.sh
+++ conncheck.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 function checkvnc1 {
-	netstat -np tcp | grep $1 > /dev/null
-	if [ "$(echo $?)" = "0" ]; then
-		echo "
-***NOTE***
-THIS IS A LOG OF CONNECTIONS WHILE YOU WERE MONITORED
-THIS DATA HOLDS INFORMATION ABOUT WHO WAS WATCHING YOU!
-******
+	conns="$(netstat -np tcp | grep $1 | tr -s ' ')"
+	ipver="$(echo $conns | cut -d' ' -f 1)"
+	if [ "$ipver" = "tcp6" ]; then
+		if [ "$(echo $conns | cut -d' ' -f 4 | cut -d'.' -f 2 | grep $1 | head -n 1)" = "$1" ]; then 
+			echo "
+###NOTE###
+# THIS IS A LOG OF CONNECTIONS WHILE YOU WERE MONITORED
+# THIS DATA HOLDS INFORMATION ABOUT WHO WAS WATCHING YOU!
 
 $(netstat -antp tcp)" >> /tmp/lb.log
-		break
+			break
+		fi
 	fi
 }
 
 function checkvnc2 {
-netstat -np tcp | grep $1 > /dev/null
-if [ "$(echo $?)" = "1" ]; then
-	break
-fi	
+	conns="$(netstat -np tcp | grep $1 | tr -s ' ')"
+	if [ "$ipver" = "tcp6" ]; then
+		if [ "$(echo $conns | cut -d' ' -f 4 | cut -d'.' -f 2 | grep $1 | head -n 1)" != "$1" ]; then 
+			break
+		fi		
+	fi	
 }
 
 function getconfarg {
